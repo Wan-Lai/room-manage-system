9c4b076cb2053f37d50c2cfc380674127ce05340
qian <1694319867@qq.com>
2022-05-26 01:03:26 +0800

    v1.0 添加测试模块

---
+++ RMS_Test/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RMS_Model;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace RMS_Dao
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            
+            Employee employee = new Employee();
+            employee.EmployeeNo = 1;
+            employee.EmployeeName = "张三";
+            employee.EmployeePassword = "1234";
+            employee.EmployeeGender = 1;
+            employee.EmployeeAge = 21;
+            employee.EmployeePosition = 1;
+            employee.EmployeeTel = "12312311313";
+            employee.EmployeeId = "64131313231231413131";
+
+            Employee_Dao.deleteEmployee(1);
+            
+        }
+    }
+}
