90e406825764a8d6c614282e8434ef700a30952a
qian <1694319867@qq.com>
2022-05-25 23:43:38 +0800

    v1.0 创建Dao类

---
+++ RMS_Dao/Employee_Dao.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RMS_Model;
+
+namespace RMS_Dao
+{
+    class UserLogin_Dao
+    {
+
+        // 添加用户
+        public static int addEmployee(Employee employee)
+        {
+            // 编号 姓名 密码 性别 年龄 职位 电话 身份证号
+            string sql = "INSERT INTO EMPLOYEE(e_no, e_name, e_password, e_gender, e_age, e_position, e_phone, e_id) VALUES(@no, @name, @password, @gender, @age, @position, @phone, @id)";
+            SqlParameter sqlParameter = new SqlParameter("@no", employee.EmployeeNo);
+
+
+            return 0;
+        }
+
+        // 查找用户
+        public static Employee selectEmployeeByName(string name)
+        {
+            string sql = "SLELCT * FROM EMPLOYEE WHERE e_name = @name";
+            SqlParameter sqlParameter = new SqlParameter("@name", name);
+            Employee employee = null;
+            using (SqlDataReader sqlDataReader = SqlHelper.executeReader(sql, sqlParameter))
+            {
+                if (sqlDataReader.Read())
+                {
+                    employee = new Employee();
+                    // 编号 姓名 密码 性别 年龄 职位 电话 身份证号
+                    employee.EmployeeNo = sqlDataReader.GetInt32(0);
+                    employee.EmployeeName = sqlDataReader.GetString(1);
+                    employee.EmployeePassword = sqlDataReader.GetString(2);
+                    employee.EmployeeGender = sqlDataReader.GetInt32(3);
+                    employee.EmployeeAge = sqlDataReader.GetInt32(4);
+                    employee.EmployeePosition = sqlDataReader.GetInt32(5);
+                    employee.EmployeeTel = sqlDataReader.GetString(6);
+                    employee.EmployeeId = sqlDataReader.GetString(7);
+                }
+            }
+            return employee;
+        }
+
+        /**
+         * 登录检测
+         *  result: {-1:未查到用户, 0:密码错误, 1:密码正确}
+         **/
+        public static int checkLogin(string name, string password)
+        {
+            int rst = 0;
+            Employee employee = UserLogin_Dao.selectEmployeeByName(name);
+            if (employee == null)
+            {
+                rst = -1;
+            }
+            else
+            {
+                if (password.Equals(employee.EmployeePassword))
+                {
+                    rst = 1;
+                }
+                else
+                {
+                    rst = 0;
+                }
+            }
+            return rst;
+        }
+    }
+}
---
+++ RMS_Dao/SqlHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RMS_Dao
+{
+    class SqlHelper
+    {
+        //1.连接字符串
+        private static readonly string constr =
+        ConfigurationManager.ConnectionStrings["connectionStr"].ConnectionString;
+        //2.执行增删改的
+        public static int ExecuteNonQuery(string sql, params SqlParameter[] pms)
+        {
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                //创建执行Sql命令对象
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        //3.执行返回单个值的
+        public static object ExecuteScalar(string sql, params SqlParameter[] pms)
+        {
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, con))
+                {
+                    if (pms != null)
+                    {
+                        cmd.Parameters.AddRange(pms);
+                    }
+                    con.Open();
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        //4.执行返回SqlDataReader
+        public static SqlDataReader ExecuteReader(string sql, params SqlParameter[] pms)
+        {
+            //创建链接对象
+            SqlConnection con = new SqlConnection(constr);
+            //创建执行命令对象
+            using (SqlCommand cmd = new SqlCommand(sql, con))
+            {
+                if (pms != null)
+                {
+                    cmd.Parameters.AddRange(pms);
+                }
+                try
+                {
+                    //打开链接
+                    con.Open();
+                    //指定操作
+                    return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                }
+                //报异常时关闭数据库销毁对象
+                catch (Exception)
+                {
+                    con.Close();
+                    con.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
