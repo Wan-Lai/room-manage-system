d6a185f8ad8c9400a4272d6c51be1596ad984eeb
qian <1694319867@qq.com>
2022-05-26 10:18:13 +0800

    v1.3 更新代码

---
+++ RMS_View/Index.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using RMS_Dao;
+using RMS_Model;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMS_View
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string username = Convert.ToString(Session["UserName"]);
+            if ((username == null)||(String.Empty.Equals(username)))
+            {
+                Response.Write("<script>alert('未登录,请登录后再来');</script>");
+                Response.Redirect("/Login.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('欢迎"+ username +"登录');</script>");
+
+                // Employee emp = Employee_Dao.selectEmployeeByName(username);
+                /*int position = emp.EmployeePosition;
+                if (position > 0)
+                {
+                    Index index = new Index();
+                    index.showAllEmployee();
+                }
+                else
+                {
+                    Response.Write("<script>alert('您的职位是" + emp.getPosition() + ",权限不足。请重新登录。');</script>");
+                    Response.Redirect("/Login.aspx");
+                }*/
+                List<Employee> emps = Employee_Dao.selectAllEmployee();
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<table border='1'>");
+                sb.Append("<tr>");
+                sb.Append("<td>编号</td>");
+                sb.Append("<td>姓名</td>");
+                sb.Append("<td>性别</td>");
+                sb.Append("<td>年龄</td>");
+                sb.Append("<td>职位</td>");
+                sb.Append("<td>电话</td>");
+                sb.Append("<td>身份证号</td>");
+                sb.Append("</tr>");
+                foreach (Employee emp in emps)
+                {
+                    sb.Append("<tr>");
+                    sb.Append("<td>" + emp.EmployeeNo + "</td>");
+                    sb.Append("<td>" + emp.EmployeeName + "</td>");
+                    sb.Append("<td>" + emp.EmployeeGender + "</td>");
+                    sb.Append("<td>" + emp.EmployeeAge + "</td>");
+                    sb.Append("<td>" + emp.EmployeePosition + "</td>");
+                    sb.Append("<td>" + emp.EmployeeTel + "</td>");
+                    sb.Append("<td>" + emp.EmployeeId + "</td>");
+                    sb.Append("</tr>");
+                }
+                sb.Append("</table>");
+                Response.Write(sb.ToString());
+            }
+        }
+
+   
+            
+    }
+}
---
+++ RMS_View/Login.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using RMS_Dao;
+
+namespace RMS_View
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            Username.Text = "";
+            Password.Text = "";
+            Response.Write("<script>alert('重置成功');</script>");
+        }
+
+        protected void Login_Click(object sender, EventArgs e)
+        {
+            string username = Username.Text.Trim();
+            string password = Password.Text.Trim();
+            switch (Employee_Dao.checkLogin(username, password))
+            {
+                case -1:
+                    Response.Write("<script>alert('账户不存在');</script>");
+                    break;
+                case 0:
+                    Response.Write("<script>alert('密码错误');</script>");
+                    break;
+                case 1:
+                    Response.Write("<script>alert('登录成功');</script>");
+                    Session["UserName"] = username;
+                    Response.Redirect("/Index.aspx");
+                    break;
+            }
+        }
+    }
+}
